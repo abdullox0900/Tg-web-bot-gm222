a59098649f41478e6172ce40f87f18a9c1c14d96
abdullox0900 <abdullox0900@gmail.com>
2022-12-24 11:34:48 +0500

    [ADD]: telegram function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useEffect } from 'react';
 import './App.css';
 
 const tg = window.Telegram.WebApp
 
 function App() {
-  return (
-    <>
 
-    </>
+  useEffect(() => {
+    tg.ready()
+  })
+
+  const onChose = () => {
+    tg.close()
+  }
+
+  return (
+    <section className='app'>
+      ISHLADI
+      <button onClick={onChose}>Закрыть</button>
+    </section>
   );
 }
 
